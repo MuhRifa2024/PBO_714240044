2271755f9588ea2ed8decdcf4fa05d1403e7ec9e
MuhRifa2024 <muhamad.rifaldi174@gmail.com>
2025-12-08 15:12:57 +0700

    Tambah p9

---
+++ pertemuan09/Praktikum/P9_7142400444/P9_7142400444/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P9_7142400444
+{
+    public partial class ULBI : Form
+    {
+        Koneksi koneksi = new Koneksi();
+
+        public void Tampil() 
+        {
+            //Query DB Get MGS
+            DataMahasiswa.DataSource = koneksi.ShowData("SELECT * FROM t_mahasiswa");
+        }
+        public ULBI()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ULBI_Load(object sender, EventArgs e)
+        {
+            Tampil();
+            DataMahasiswa.Columns[0].HeaderText = "NPM";
+            DataMahasiswa.Columns[1].HeaderText = "Nama";
+            DataMahasiswa.Columns[2].HeaderText = "Angkatan";
+            DataMahasiswa.Columns[3].HeaderText = "Alamat";
+            DataMahasiswa.Columns[4].HeaderText = "Email";
+            DataMahasiswa.Columns[5].HeaderText = "No Hp";
+        }
+    }
+}
